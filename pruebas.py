3cfbd0c2a235191a50f5c517d724a97586d1497c
juanrd0088 <juanrd0088@users.noreply.github.com>
2018-10-29 17:13:25 +0100

    Add files via upload

---
+++ pruebas.py
@@ -0,0 +1,41 @@
+import re
+
+
+def funcion1():
+	hola = 34
+	return "pepito"
+
+def fun2	(cadena,num):
+	hola()
+	for i in range(3):
+		print(cadena+" def adios for i in range3000")
+		funcion1()
+		funcion1()
+		for j in range(2):
+			cosa()
+			for k in range(5):
+				main()
+	funcion1()
+	hola()
+
+def main():
+	fun2("hola",10)
+	return 0
+
+#comentario
+#def adios2():
+def cosa( def adios():):
+	for i in range(25):
+		hola()
+ 
+def hola():
+	pass
+
+def cosa():
+	pass
+
+def test():
+	print("HOLA")
+	return "done"
+	return "redone"
+
